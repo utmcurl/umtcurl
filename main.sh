19ea0421ffbb9c73f015043a7966bc367e11434f
Dirk <surfrightbv@debian.localdomain>
2010-08-09 10:26:30 -0400

    +Usage; now usable with --help etc +Arguments passing on the command line
    *Loads all the files in the given directory
    *Order of checking changed
    
    @TODO:
    Checking if passed argument ends with /; gives problems laters on with loading
    *

--- main.sh
+++ main.sh
@@ -1,26 +1,38 @@
 #! /bin/sh
 
-
-#Checks if there is a given argument that represents the directory which contains the scripts
-#Set a variable which represents the directory which contains the files of the tests
-#This is more an extra, it doesn't support all cases and basicly you only want to use the current directory
-if [ "$1" = "" ] #Empty?
+usageProvider ()
+{
+if [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$1" == "" ] || [ "$1" == " " ] #Print some usage info
 then
-	FILES=$PWD/* #Use the current directory -not all supporting
-	EXECUTABLEPATH=
-else
-	FILES=$1/* #Use the given directory -use on own "risk"
-	
-	if [ "$1" == "--help" || "$1" == "-h" || "$1" == "-?" ] #Print some usage info
-	EXECUTABLEPATH=
+	echo "Usage: sh main.sh [directory of test scripts] [executable path of proxy]"
+	echo "      [sh main.sh /home/surfrightbv/testfiles /home/sufrightbv/program/proxy]"
+	echo ""
+	echo "Commands:"
+	echo "	--help | -h | -? | - This help"
+fi
 
+}
+
+
+
+
+if [ "$1" != "" ] && [ "$2" != "" ] 
+then
+#Set some variables which represents the directory which contains the files of the tests and the location of the binary of the executable
+#Simply add /* to get all the files inside the directory, works with /home/test and /home/test/ but not with /home/test/*
+	DIRECTORY=$1/*
+	EXECUTABLEPATH=$2
+	
+	echo "Directory: $DIRECTORY"
+	echo "Executable: $EXECUTABLEPATH"
+
+	for f in $DIRECTORY
+	do
+		echo $f
+	done
+else
+	usageProvider
 fi
 
 
-if [ "$1" == "--help" || "$1" == "-h" || "$1" == "-?" ] #Print some usage info
-
-for f in $FILES
-do
-  echo "Processing $f file..."
-done
 
